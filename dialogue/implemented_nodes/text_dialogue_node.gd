38798d509a104fa8b070dd26dad1ed2093e545bb
Peter Vasilyev <p.vasilyev@inbox.ru>
2022-11-26 12:23:55 +0300

    #114 Remove "dummy text"

--- dialogue/implemented_nodes/text_dialogue_node.gd
+++ dialogue/implemented_nodes/text_dialogue_node.gd
@@ -3,7 +3,7 @@ class_name TextDialogueNode
 extends DialogueNode
 
 
-export(String) var text := "dummy text" setget set_text
+export(String) var text := "" setget set_text
 export(Resource) var tags setget set_tags
 # StorageItem
 export(Resource) var speaker: Resource setget set_speaker
@@ -28,7 +28,7 @@ func set_tags(new_tags: Storage) -> void:
     tags = new_tags
     emit_signal("contents_changed")
 
-    
+
 func set_speaker(new_speaker: Resource) -> void:
     if new_speaker != speaker:
         speaker = new_speaker
@@ -43,11 +43,11 @@ func set_listener(new_listener: Resource) -> void:
 
 func clone() -> Clonable:
     var copy = .clone()
-    
+
     copy.tags = tags.clone()
     if speaker:
         copy.speaker = speaker.clone()
     if listener:
         copy.listener = listener.clone()
-    
+
     return copy
