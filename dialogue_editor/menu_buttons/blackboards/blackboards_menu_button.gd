81988291df62deb6c643d7f7473986f1ece2793c
Peter Vasilyev <p.vasilyev@inbox.ru>
2023-03-15 09:44:04 +0300

    #105 Change Blackboards menu item placement

--- dialogue_editor/menu_buttons/blackboards/blackboards_menu_button.gd
+++ dialogue_editor/menu_buttons/blackboards/blackboards_menu_button.gd
@@ -7,7 +7,7 @@ signal open_blackboard_editor()
 
 
 func _ready() -> void:
-    _add_button("Edit Dialogue Blackboards...", "open_dialogue_blackboards_editor")
-    _popup.add_separator()
     _add_button("Edit Blackboard...", "open_blackboard_editor")
+    _popup.add_separator()
+    _add_button("Edit Dialogue Blackboards...", "open_dialogue_blackboards_editor")
 
