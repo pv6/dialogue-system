2d78ba11ae9c0e1eb959186a8d70ec94b914ff5d
Peter Vasilyev <p.vasilyev@inbox.ru>
2023-10-01 12:50:23 +0300

    #240 Change text field unfocus hotkey to Enter

--- dialogue_editor/support/unfocusable_text_edit.gd
+++ dialogue_editor/support/unfocusable_text_edit.gd
@@ -7,10 +7,10 @@ var _session: DialogueEditorSession = preload("res://addons/dialogue_system/dial
 
 
 func _input(event: InputEvent) -> void:
-    if not event is InputEventKey or get_focus_owner() != self:
+    if not event is InputEventKey or not has_focus():
         return
 
-    if event.scancode == KEY_ESCAPE or event.scancode == KEY_ENTER and Input.is_key_pressed(KEY_CONTROL):
+    if event.scancode == KEY_ESCAPE or event.is_pressed() and event.scancode == KEY_ENTER and not Input.is_key_pressed(KEY_CONTROL):
         release_focus()
         _session.focus_graph_renderer()
 
