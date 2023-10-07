42923c7a17de553c42219e10fa6083d77b384d14
Peter Vasilyev <p.vasilyev@inbox.ru>
2023-10-07 19:43:35 +0300

    #167 Fix auto-flags not being hidden on older version dialogue files

--- support/storage.gd
+++ support/storage.gd
@@ -12,9 +12,10 @@ export(Dictionary) var _data: Dictionary
 # this is supposed to be a set, but godot doesn't have those
 export(Dictionary) var _locked_ids: Dictionary
 # this is supposed to be a set, but godot doesn't have those
-export(Dictionary) var _hidden_ids: Dictionary
+export(Dictionary) var _hidden_ids: Dictionary setget _set_hidden_ids
+
 # this is supposed to be a set, but godot doesn't have those
-export(Dictionary) var _shown_ids: Dictionary setget _set_shown_ids
+var _shown_ids: Dictionary
 
 var _uid_generator := UIDGenerator.new()
 
@@ -38,6 +39,14 @@ func get_name() -> String:
 func _init() -> void:
     _data = {}
     _locked_ids = {}
+    _shown_ids = {}
+
+
+func _set_hidden_ids(new_hidden_ids: Dictionary) -> void:
+    _hidden_ids = new_hidden_ids.duplicate()
+    _shown_ids = _data.duplicate()
+    for hidden_id in _hidden_ids.keys():
+        _shown_ids.erase(hidden_id)
 
 
 func add_item(new_item, id := UIDGenerator.DUMMY_ID) -> int:
@@ -48,7 +57,9 @@ func add_item(new_item, id := UIDGenerator.DUMMY_ID) -> int:
         id = _uid_generator.generate_id()
     assert(not _data.has(id))
 
+    _shown_ids[id] = true
     _set_item(id, new_item)
+
     emit_changed()
     return id
 
@@ -147,15 +158,6 @@ func shown_ids() -> Array:
     return _shown_ids.keys()
 
 
-func _set_shown_ids(new_shown_ids: Dictionary) -> void:
-    _shown_ids = new_shown_ids
-
-    if _shown_ids.size() != _data.size() - _hidden_ids.size():
-        _shown_ids = _data.duplicate()
-        for hidden_id in _hidden_ids.keys():
-            _shown_ids.erase(hidden_id)
-
-
 func items() -> Array:
     return _data.values()
 
@@ -219,3 +221,4 @@ func _remove_item(id: int) -> void:
     assert(_data.has(id))
     _set_item(id, null)  # disconnect from 'changed' signal of item
     _data.erase(id)
+    _shown_ids.erase(id)
